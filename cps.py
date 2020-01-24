4805aaac33acb20b559e5006daaeeaf151cc37e3
Timothy Sanders <timothyasanders@gmail.com>
2020-01-24 13:09:18 +0100

    made changes to the error messages and added just a card number reader for when new customers signup we can figure out the card number

--- cps.py
+++ cps.py
@@ -22,13 +22,25 @@ def readRFID():
                 ID = ID + str(read_byte)
     return ID
 
+def getserial():
+  # Extract serial from cpuinfo file
+  cpuserial = "0000000000000000"
+  try:
+    f = open('/proc/cpuinfo','r')
+    for line in f:
+      if line[0:6]=='Serial':
+        cpuserial = line[10:26]
+    f.close()
+  except:
+    cpuserial = "ERROR000000000"
 
-print("List of commands: (R)ead(Ca)rd, (Ex)it ")
+  return cpuserial
 
+print("List of commands: (M)ake(P)urchase, (Ex)it, (R)ead(C)ard(N)umber ")
 
 while(True):
     cmd = raw_input("Please enter a command: ")
-    if(cmd.lower() == 'rca' or cmd.lower == ("ReadCard")):
+    if(cmd.lower() == 'mp' or cmd.lower == ("MakePurchase")):
         purAmnt = raw_input("Please enter the purchase amount:")
         camera.start_preview()
         card_id = readRFID()
@@ -38,10 +50,10 @@ while(True):
         fileName = str(card_id) + "_" + tDateStr + "_" + str(randNum) +'.jpg'
         fileNamePath = '/home/pi/Pictures/'+ fileName
         #will add custnum and cardnum
+        sleep(2)
         camera.capture(fileNamePath)
         camera.stop_preview()
         print("Authenticating...")
-        print('filename=',fileName)
         #send photo to server
         multipart_form_data = {
             'file': (fileName, open(fileNamePath, 'rb')),
@@ -49,17 +61,32 @@ while(True):
             'origFileName': (None, fileName),
             'cardId': (None,card_id),
             'storeId': (None,store_id),
-            'purAmnt': (None, purAmnt)
+            'purAmnt': (None, purAmnt),
+            'terminalNum': (None, getserial())
         }
 
-        response = requests.post('http://etu-web2.ut-capitole.fr:3013/aws/transaction', files=multipart_form_data)
-        rJson = response.json()
+        try:
+            response = requests.post('http://etu-web2.ut-capitole.fr:3013/aws/transaction', files=multipart_form_data)
+        except:
+            print("There was an error establishing a connection with the server...")
 
-        if(rJson.match == "success"):
+        rJson = response.json()
+        #print("response json",rJson)
+
+        if(rJson['match'] == "success"):
             print("Your transaction was successful")
-        elif(rJson.match == "fail"):
+        elif(rJson['match'] == "fail"):
             print("Authentication failed.")
+        elif(rJson['match'] == "ERROR"):
+            print("There was an error processing the transaction")
+            print("Error message: %s " % (rJson['value']) )
 
         card_id = ""
+
+    elif (cmd.lower() == 'rcn' or cmd.lower() == 'ReadCardNumber'):
+        cardNum = readRFID()
+        print("Your card number is: %s" % (cardNum))
     elif(cmd.lower() == 'ex' or cmd.lower() == 'exit'):
         break
+
+
